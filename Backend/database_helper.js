f5c6440be99b26511676260bda7e6a44a502d396
Sebastian Lindmark <sebbe.lindmark@gmail.com>
2018-04-17 16:44:53 +0200

    Add initial structure of backend

---
+++ Backend/database_helper.js
@@ -0,0 +1,12 @@
+const sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('./db.sqlite3');
+
+module.exports = {
+    database : require("./db_helpers/db_conn"),
+    user : require("./db_helpers/user"),
+
+
+    reset_database : function(){
+        this.database.drop_tables();
+    }
+};
--- Backend/server.js
+++ Backend/server.js
@@ -1,28 +1,76 @@
 const express = require('express');
+const sqlite3 = require('sqlite3');
+
+//var db = new sqlite3.Database('./db.sqlite3');
+var bodyParser = require('body-parser')
+
+var database_helper = require("./database_helper");
+database_helper.reset_database();
+
+var sqliteJson = require("sqlite-json")
+exporter = sqliteJson('./db.sqlite3');
+
 const app = express();
 
 var hostPort = 8000;
 
-var mongoClient = require('mongodb').MongoClient;
-var mongoUrl = "mongodb://localhost:27017/database";
 
+/*function create_database(){
+//db.run('DROP TABLE IF EXISTS user;')
+//db.run('CREATE TABLE user(id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, username TEXT, password TEXT);');	
+}*/
+
+
+exporter.json('select * FROM user', function (err, json) {
+  //console.log(json);
+  //console.log(err);
+});
 
 
 app.route('/').get((req, res) => {
     res.send("Welcome")
 });
 
-app.route('/hello').get((req, res) => {
-    res.send("Snopp :)")
+app.route('/api/user/create').get((req, res) => {
+
+    var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;	
+    database_helper.user.insert_user(email,username,password);	
+    res.send("")
+    
 });
 
-mongoClient.connect(mongoUrl,function(err, db) {
-    if (err) throw err;
-    console.log("Database created!");
-    db.close();
+
+app.route('/api/user/get').get((req, res) => {
+        var username = req.body.username;
+        database_helper.user.get_user(username, function(response) {
+            res.send(response);
+    });	
+});
+
+
+app.route('/api/test/sign_up').get((req,res) => {
+    database_helper.user.insert_user("sebbe@gmail.com","sebbe","passw");	
+});
+
+/*app.route('/api/test/show').get((req,res) => {
+    console.log("In API/TEST/SHOW");
+    //console.log(database_helper.user.get_user("sebbe"));
+    res.send("JApp :)");
+});*/
+
+
+app.route('/api/sign_up').get((req, res) => {
+    var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;	
+    database_helper.user.insert_user("sebbe@gmail.com","sebbe","passw");	
 });
 
 
 app.listen(hostPort, () => {
     console.log('Server started!');
   });
+  
+  
