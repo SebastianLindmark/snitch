68f6c3dcc35d1e07af1fd55068270bf5a1643dbb
Andreas Järvelä <andja235@student.liu.se>
2018-05-14 13:38:17 +0200

    Update settings.service

--- Frontend/src/app/_services/settings.service.ts
+++ Frontend/src/app/_services/settings.service.ts
@@ -17,22 +17,9 @@ export class SettingsService {
   getStreamKey(){
     var request = this.http.post<User>(this.BASE_URL + '/protected/get_stream_key',{});
     return request.map((res : any) => {  
-<<<<<<< HEAD
-        return res.key;
-      }
-    );
-  }
-  changeUsername(username : string){
-      var request = this.http.post<User>(this.BASE_URL + '/change_username', {username : username});
-      return request.map((res : any) => {
-        return res;
-      });
-  }
-=======
       return res.result.key;
     }
   );
 }
->>>>>>> 646f02d038a1224662b68a54d1e553bcaad6954e
 
 }
