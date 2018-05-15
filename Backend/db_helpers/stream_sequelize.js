ff11d8d4b5ea21fabee73c3b9779c8bea205d1cc
Sebastian Lindmark <sebbe.lindmark@gmail.com>
2018-05-15 16:01:01 +0200

    Remove Stream and replace with StreamConfig, also create a new StreamConfig for each new user

--- Backend/db_helpers/stream_sequelize.js
+++ Backend/db_helpers/stream_sequelize.js
@@ -3,5 +3,33 @@ var models = require("./models");
 module.exports = {
     create_stream : function(user) {
         models.Stream.create({})        
+    },
+
+
+    set_stream_online : function(user,game,title){
+
+        return user.getStreamConfig().then(function(streamConfig){
+            return streamConfig.updateAttributes({
+                live: true
+            }) 
+        }).then(function(streamConfig){
+            
+    })},
+
+    
+    update_stream_config : function(user,game, title){
+
+    },
+
+
+    set_stream_offline : function(user){
+        return user.getStreamConfig().then(function(streamConfig){
+            return streamConfig.updateAttributes({
+                live: false
+            }) 
+        }).then(function(streamConfig){
+            
+    })
     }
+
 }
