ca739887c90110a4a164a7c10d586270520c831d
Andreas Järvelä <andja235@student.liu.se>
2018-06-03 09:07:04 +0200

    Remove unused files, old commit forgot to push

--- Backend/chat-server.js
+++ Backend/chat-server.js
@@ -5,24 +5,12 @@ const port = 8040
 
 function onConnect (service, id) {
     // Assuming that auth data is passed in a query string.
- 
     let { query } = service.transport.getHandshakeData(id)
     let { token } = query;
-
     var decoded =  jwt.verify(token, 'secret');
     let username = decoded.username;
-    return Promise.resolve(username)
+    return Promise.resolve(username);
 }
 
 const chatService = new ChatService({port}, {onConnect})
-//chatService.config.enableRoomsManagement = true;
 chatService.enableRoomsManagement = true;
-
-
-chatService.hasRoom('default').then(hasRoom => {
-    if (!hasRoom) {
-        return chatService.addRoom('default', { owner: 'admin' })
-}
-
-    
-})
